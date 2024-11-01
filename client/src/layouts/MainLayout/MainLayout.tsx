98aa7a0dce2e7334918c520e196cd7a71d993319
Marian Smolii <smoliimarian@gmail.com>
2024-11-01 11:41:44 +0200

    feat: add aside to main layout

--- client/src/layouts/MainLayout/MainLayout.tsx
+++ client/src/layouts/MainLayout/MainLayout.tsx
@@ -1,16 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import styles from "./MainLayout.module.scss";
 import Header from "../../components/Header/Header";
+import useScreenSize from "../../hooks/useScreenSize";
 import Container from "../../components/Container/Container";
+import Aside from "../../components/Aside/Aside";
 
 const MainLayout = () => {
+  const { pathname } = useLocation();
+  const { width } = useScreenSize();
+
+  const showAside = ["/", "/profile"].includes(pathname) && width >= 1440;
+
   return (
     <>
       <Header />
 
       <main className={styles.main}>
         <Container className={styles.container}>
+          {showAside && <Aside />}
+
           <Outlet />
         </Container>
       </main>
