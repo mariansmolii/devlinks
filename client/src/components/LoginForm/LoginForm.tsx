4a2247960e8f5843faffdc716c469f2f6535e13e
Marian Smolii <smoliimarian@gmail.com>
2024-11-14 00:04:27 +0200

    feat: create auth forms

---
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,117 @@
+import Input from "../Input/Input";
+import Label from "../Label/Label";
+import toast from "react-hot-toast";
+import Button from "../Button/Button";
+import useAuth from "../../hooks/useAuth";
+import BtnLoader from "../Loader/BtnLoader";
+import ErrorMsg from "../ErrorMsg/ErrorMsg";
+import CustomToast from "../CustomToast/CustomToast";
+import styles from "./LoginForm.module.scss";
+
+import { z } from "zod";
+import { Err } from "../../types/auth";
+import { useAppDispatch } from "../../hooks/useRedux";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { login } from "../../store/auth/authOperations";
+import { loginSchema } from "../../utils/validations/auth";
+
+const LoginForm = () => {
+  const { isLoading } = useAuth();
+  const dispatch = useAppDispatch();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    reset,
+  } = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = async ({ email, password }: z.infer<typeof loginSchema>) => {
+    if (!email.trim() || !password.trim()) return;
+
+    try {
+      await dispatch(login({ email, password })).unwrap();
+
+      reset();
+    } catch (error) {
+      const err = error as Err;
+
+      toast.custom((t) => (
+        <CustomToast t={t} text={`${err?.message}!`} icon={"warning"} />
+      ));
+    }
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
+      <div>
+        <Label
+          id="loginEmail"
+          label="Email address"
+          error={errors.email?.message}
+        />
+
+        <Controller
+          name="email"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="loginEmail"
+              type="email"
+              placeholder="e.g. alex@email.com"
+              iconName="icon-email"
+              error={errors.email?.message}
+              {...field}
+            />
+          )}
+        />
+
+        {errors.email && <ErrorMsg message={errors.email.message} />}
+      </div>
+
+      <div>
+        <Label
+          id="loginPassword"
+          label="Password"
+          error={errors.password?.message}
+        />
+
+        <Controller
+          name="password"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="loginPassword"
+              type="password"
+              placeholder="Enter your password"
+              iconName="icon-password"
+              error={errors.password?.message}
+              {...field}
+            />
+          )}
+        />
+
+        {errors.password && <ErrorMsg message={errors.password.message} />}
+      </div>
+
+      <Button
+        title={isLoading ? <BtnLoader /> : "Login"}
+        disabled={isLoading}
+        variant={"primary"}
+        type="submit"
+      />
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ client/src/components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,156 @@
+import Input from "../Input/Input";
+import Label from "../Label/Label";
+import toast from "react-hot-toast";
+import Button from "../Button/Button";
+import useAuth from "../../hooks/useAuth";
+import BtnLoader from "../Loader/BtnLoader";
+import ErrorMsg from "../ErrorMsg/ErrorMsg";
+import styles from "./RegisterForm.module.scss";
+import CustomToast from "../CustomToast/CustomToast";
+
+import { z } from "zod";
+import { Err } from "../../types/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useAppDispatch } from "../../hooks/useRedux";
+import { useForm, Controller } from "react-hook-form";
+import { register } from "../../store/auth/authOperations";
+import { registerSchema } from "../../utils/validations/auth";
+
+const RegisterForm = () => {
+  const { isLoading } = useAuth();
+  const dispatch = useAppDispatch();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    reset,
+  } = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = async ({
+    email,
+    password,
+    confirmPassword,
+  }: z.infer<typeof registerSchema>) => {
+    if (!email.trim() || !password.trim() || !confirmPassword.trim()) return;
+
+    try {
+      await dispatch(register({ email, password })).unwrap();
+
+      toast.custom((t) => (
+        <CustomToast t={t} text="Registration is successful!" icon={"check"} />
+      ));
+
+      reset();
+    } catch (error) {
+      const err = error as Err;
+
+      toast.custom((t) => (
+        <CustomToast t={t} text={`${err?.message}!`} icon={"warning"} />
+      ));
+    }
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
+      <div>
+        <Label
+          id="registerEmail"
+          label="Email address"
+          error={errors.email?.message}
+        />
+
+        <Controller
+          name="email"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="registerEmail"
+              type="email"
+              placeholder="e.g. alex@email.com"
+              iconName="icon-email"
+              error={errors.email?.message}
+              {...field}
+            />
+          )}
+        />
+
+        {errors.email && <ErrorMsg message={errors.email.message} />}
+      </div>
+
+      <div>
+        <Label
+          id="registerPassword"
+          label="Password"
+          error={errors.password?.message}
+        />
+
+        <Controller
+          name="password"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="registerPassword"
+              type="password"
+              placeholder="At least 8 characters"
+              iconName="icon-password"
+              error={errors.password?.message}
+              {...field}
+            />
+          )}
+        />
+
+        {errors.password && <ErrorMsg message={errors.password.message} />}
+      </div>
+
+      <div>
+        <Label
+          id="confirmPassword"
+          label="Confirm password"
+          error={errors.confirmPassword?.message}
+        />
+
+        <Controller
+          name="confirmPassword"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="confirmPassword"
+              type="password"
+              placeholder="At least 8 characters"
+              iconName="icon-password"
+              error={errors.confirmPassword?.message}
+              {...field}
+            />
+          )}
+        />
+
+        {errors.confirmPassword && (
+          <ErrorMsg message={errors.confirmPassword.message} />
+        )}
+      </div>
+
+      <p className={styles.text}>Password must contain at least 8 characters</p>
+
+      <Button
+        title={isLoading ? <BtnLoader /> : "Create new account"}
+        disabled={isLoading}
+        variant={"primary"}
+        type="submit"
+      />
+    </form>
+  );
+};
+
+export default RegisterForm;
