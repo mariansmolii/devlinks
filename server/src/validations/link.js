0baa563635ed013e4a597a9405a469115383275a
Marian Smolii <smoliimarian@gmail.com>
2024-11-17 11:57:46 +0200

    fix: link validation schema

--- server/src/validations/link.js
+++ server/src/validations/link.js
@@ -1,12 +1,19 @@
 import Joi from "joi";
 
 export const linkSchema = Joi.object({
-  url: Joi.string().uri().required(),
-  platform: Joi.object({
-    value: Joi.string().required(),
-    label: Joi.string().required(),
-    iconName: Joi.string().required(),
-    color: Joi.string().required(),
-  }).required(),
-  index: Joi.number().required(),
+  links: Joi.array()
+    .items(
+      Joi.object({
+        url: Joi.string().uri().required(),
+        platform: Joi.object({
+          value: Joi.string().required(),
+          label: Joi.string().required(),
+          iconName: Joi.string().required(),
+          color: Joi.string().required(),
+        }).required(),
+        index: Joi.number().integer().min(0).required(),
+      })
+    )
+    .min(1)
+    .required(),
 });
