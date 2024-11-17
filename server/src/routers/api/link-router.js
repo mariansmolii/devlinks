52a0a3fe4ab804c04660ed20d339922c2ca1400e
Marian Smolii <smoliimarian@gmail.com>
2024-11-17 12:25:52 +0200

    refactor: refactor delete link router

--- server/src/routers/api/link-router.js
+++ server/src/routers/api/link-router.js
@@ -4,11 +4,7 @@ import linkController from "../../controllers/link-controller.js";
 
 import { linkSchema } from "../../validations/link.js";
 import { validateBody } from "../../decorators/index.js";
-import {
-  authenticate,
-  isEmptyBody,
-  isValidId,
-} from "../../middlewares/index.js";
+import { authenticate, isEmptyBody } from "../../middlewares/index.js";
 
 const linkRouter = express.Router();
 
@@ -20,6 +16,6 @@ linkRouter.get("/", linkController.getAllLinks);
 
 linkRouter.patch("/save", isEmptyBody, linkValidate, linkController.saveLinks);
 
-linkRouter.delete("/:linkId", isValidId, linkController.deleteLink);
+linkRouter.delete("/remove", isEmptyBody, linkController.deleteLink);
 
 export default linkRouter;
