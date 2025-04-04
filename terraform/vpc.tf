64a88a9459e2a66261271308e4a467672760bdb6
Marian Smolii <smoliimarian@gmail.com>
2025-04-04 17:29:01 +0300

    feat: change subnet ip cidr range

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,22 +2,22 @@ resource "google_compute_network" "vpc_network" {
   name                    = "${var.project_id}-vpc"
   auto_create_subnetworks = false
 
-  depends_on = [ google_project_service.api ]
+  depends_on = [google_project_service.api]
 }
 
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
   region        = var.region
   network       = google_compute_network.vpc_network.name
-  ip_cidr_range = "10.0.0.0/24"
+  ip_cidr_range = var.subnet_cidr_range
 
   secondary_ip_range {
     range_name    = "pods-range"
-    ip_cidr_range = "10.1.0.0/24"
+    ip_cidr_range = "10.1.0.0/20"
   }
 
   secondary_ip_range {
     range_name    = "services-range"
-    ip_cidr_range = "10.2.0.0/24"
+    ip_cidr_range = "10.2.0.0/20"
   }
 }
