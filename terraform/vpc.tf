b652136e26673cad46b22e49b4a026eee4928457
Marian Smolii <smoliimarian@gmail.com>
2025-04-05 18:03:58 +0300

    feat: create private subnet with nat

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,15 +1,17 @@
-resource "google_compute_network" "vpc_network" {
+resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
+  routing_mode            = "REGIONAL"
   auto_create_subnetworks = false
 
   depends_on = [google_project_service.api]
 }
 
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc_network.name
-  ip_cidr_range = var.subnet_cidr_range
+resource "google_compute_subnetwork" "private" {
+  name                     = "private-subnet"
+  ip_cidr_range            = var.subnet_cidr_range
+  region                   = var.region
+  network                  = google_compute_network.vpc.name
+  private_ip_google_access = true
 
   secondary_ip_range {
     range_name    = "pods-range"
@@ -19,5 +21,34 @@ resource "google_compute_subnetwork" "subnet" {
   secondary_ip_range {
     range_name    = "services-range"
     ip_cidr_range = "10.2.0.0/20"
+  }
+}
+
+resource "google_compute_address" "nat" {
+  name         = "nat"
+  address_type = "EXTERNAL"
+  network_tier = "PREMIUM"
+
+  depends_on = [google_project_service.api]
+}
+
+resource "google_compute_router" "router" {
+  name    = "router"
+  region  = var.region
+  network = google_compute_network.vpc.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name   = "nat"
+  region = var.region
+  router = google_compute_router.router.name
+
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ips                            = [google_compute_address.nat.self_link]
+
+  subnetwork {
+    name                    = google_compute_subnetwork.private.self_link
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 }
