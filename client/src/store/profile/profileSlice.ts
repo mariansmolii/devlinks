63f818e62d53b52dfc1183c939049d7accbd3c53
Marian Smolii <smoliimarian@gmail.com>
2025-04-14 14:02:40 +0300

    feat: add profile data to store

--- client/src/store/profile/profileSlice.ts
+++ client/src/store/profile/profileSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { PersonalDetails, ProfileState } from "../../types/profile";
+import { handleFulfilled, handlePending, handleRejected } from "../handlers";
+import { getProfileInfo, updateProfileInfo } from "./profileOperations";
 
 const initialState: ProfileState = {
   formData: {
@@ -30,6 +32,31 @@ const profileSlice = createSlice({
     setProfileImagePreview: (state, { payload }: { payload: string }) => {
       state.formData.profileImage.previewImage = payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(updateProfileInfo.fulfilled, (state, { payload }) => {
+        state.formData.personalDetails = {
+          ...state.formData.personalDetails,
+          ...payload,
+        };
+      })
+      .addCase(getProfileInfo.fulfilled, (state, { payload }) => {
+        state.formData.personalDetails = {
+          ...state.formData.personalDetails,
+          ...payload,
+        };
+        state.formData.profileImage.savedImage = payload.profileImage ?? null;
+      })
+      .addMatcher(
+        (action) => action.type.endsWith("/fulfilled"),
+        handleFulfilled
+      )
+      .addMatcher((action) => action.type.endsWith("/pending"), handlePending)
+      .addMatcher(
+        (action) => action.type.endsWith("/rejected"),
+        handleRejected
+      );
   },
 });
 
