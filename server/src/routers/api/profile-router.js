84bbcaf244af7f196ac11939b696a47c54672567
Marian Smolii <smoliimarian@gmail.com>
2025-04-15 21:54:40 +0300

    feat: add profile validation

--- server/src/routers/api/profile-router.js
+++ server/src/routers/api/profile-router.js
@@ -2,13 +2,20 @@ import express from "express";
 import profileController from "../../controllers/profile-controller.js";
 
 import { authenticate } from "../../middlewares/index.js";
+import { validateBody } from "../../decorators/index.js";
+import { updateProfileSchema } from "../../validations/profile.js";
 
 const profileRouter = express.Router();
+const profileValidate = validateBody(updateProfileSchema);
 
 profileRouter.use(authenticate);
 
 profileRouter.get("/", profileController.getProfile);
 
-profileRouter.patch("/update", profileController.updateProfileInfo);
+profileRouter.patch(
+  "/update",
+  profileValidate,
+  profileController.updateProfileInfo
+);
 
 export default profileRouter;
