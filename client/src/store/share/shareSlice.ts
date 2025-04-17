a632c200c0c4672f541a8fa0f52b6ccfab06f740
Marian Smolii <smoliimarian@gmail.com>
2025-04-18 00:01:16 +0300

    feat: create share slice

---
+++ client/src/store/share/shareSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { ShareState } from "../../types/share";
+import { handleFulfilled, handlePending, handleRejected } from "../handlers";
+import { getSharedData } from "./shareOperations";
+
+const initialState: ShareState = {
+  firstName: "",
+  lastName: "",
+  profileEmail: "",
+  profileImage: null,
+  links: [],
+  isLoading: false,
+  error: null,
+};
+
+const shareSlice = createSlice({
+  name: "share",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getSharedData.fulfilled, (state, { payload }) => {
+        state.firstName = payload.firstName;
+        state.lastName = payload.lastName;
+        state.profileEmail = payload.profileEmail;
+        state.profileImage = payload.profileImage;
+        state.links = payload.links;
+      })
+      .addMatcher(
+        (action) => action.type.endsWith("/fulfilled"),
+        handleFulfilled
+      )
+      .addMatcher((action) => action.type.endsWith("/pending"), handlePending)
+      .addMatcher(
+        (action) => action.type.endsWith("/rejected"),
+        handleRejected
+      );
+  },
+});
+
+export const shareReducer = shareSlice.reducer;
