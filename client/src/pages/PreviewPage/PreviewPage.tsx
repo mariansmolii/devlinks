9ee339335fa0e57befc4dd0e526fb7d3b9a74808
Marian Smolii <smoliimarian@gmail.com>
2025-04-17 13:47:51 +0300

    feat: add props to ProfileDataPreview

--- client/src/pages/PreviewPage/PreviewPage.tsx
+++ client/src/pages/PreviewPage/PreviewPage.tsx
@@ -1,11 +1,14 @@
+import useLink from "../../hooks/useLink";
 import styles from "./PreviewPage.module.scss";
 import useProfile from "../../hooks/useProfile";
 import PreviewLinkList from "../../components/PreviewLinkList/PreviewLinkList";
 import ProfileDataPreview from "../../components/ProfileDataPreview/ProfileDataPreview";
 
 const PreviewPage = () => {
+  const { links } = useLink();
   const {
     profileImage: { previewImage, savedImage },
+    personalDetails: { firstName, lastName, profileEmail },
   } = useProfile();
 
   return (
@@ -23,8 +26,18 @@ const PreviewPage = () => {
           <div />
         )}
 
-        <ProfileDataPreview showImage={false} />
-        <PreviewLinkList className={styles.links} showAll={true} />
+        <ProfileDataPreview
+          showImage={false}
+          firstName={firstName}
+          lastName={lastName}
+          profileEmail={profileEmail ?? ""}
+        />
+
+        <PreviewLinkList
+          links={links}
+          className={styles.links}
+          showAll={true}
+        />
       </div>
     </>
   );
