5bc6a29398a09ff6479524681ffdd91a367b17f9
Marian Smolii <smoliimarian@gmail.com>
2025-04-27 13:44:02 +0300

    feat: add kubernetes provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,10 +4,31 @@ terraform {
       source  = "hashicorp/google"
       version = "6.27.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.36.0"
+    }
   }
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
+}
+
+data "google_container_cluster" "gke" {
+  name     = "${var.project_id}-gke"
+  location = var.zone
+
+  depends_on = [
+    google_container_cluster.primary
+  ]
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.gke.endpoint}"
+  cluster_ca_certificate = base64decode(data.google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
 }
