36baf7c91f09ca786d32d5f55e8396c33abb9fb0
Marian Smolii <smoliimarian@gmail.com>
2025-05-09 16:20:35 +0300

    fix: add check if user is loggedIn

--- client/src/layouts/MainLayout/MainLayout.tsx
+++ client/src/layouts/MainLayout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import useAuth from "../../hooks/useAuth";
 import styles from "./MainLayout.module.scss";
 import Aside from "../../components/Aside/Aside";
 import Header from "../../components/Header/Header";
@@ -7,9 +8,11 @@ import { Outlet, useLocation } from "react-router-dom";
 
 const MainLayout = () => {
   const { pathname } = useLocation();
+  const { isLoggedIn } = useAuth();
 
-  const showAside = ["/", "/profile"].includes(pathname);
-  const showHeader = ["/", "/profile", "/preview"].includes(pathname);
+  const showAside = isLoggedIn && ["/", "/profile"].includes(pathname);
+  const showHeader =
+    isLoggedIn && ["/", "/profile", "/preview"].includes(pathname);
 
   return (
     <>
